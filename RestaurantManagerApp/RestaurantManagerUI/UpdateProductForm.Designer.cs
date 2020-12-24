47c447f3c1c303e47f29b179289768dc496e3f73
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 14:16:59 +0200

    Created update product form

---
+++ RestaurantManagerApp/RestaurantManagerUI/UpdateProductForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace RestaurantManagerUI
+{
+    partial class UpdateProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.updateProductButton = new System.Windows.Forms.Button();
+            this.portionSizeValue = new System.Windows.Forms.TextBox();
+            this.portionSizeLabel = new System.Windows.Forms.Label();
+            this.unitValue = new System.Windows.Forms.TextBox();
+            this.unitLabel = new System.Windows.Forms.Label();
+            this.portionCountValue = new System.Windows.Forms.TextBox();
+            this.portionCountLabel = new System.Windows.Forms.Label();
+            this.productNameValue = new System.Windows.Forms.TextBox();
+            this.productNameLabel = new System.Windows.Forms.Label();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // updateProductButton
+            // 
+            this.updateProductButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.updateProductButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.updateProductButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.updateProductButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.updateProductButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateProductButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.updateProductButton.Location = new System.Drawing.Point(39, 451);
+            this.updateProductButton.Name = "updateProductButton";
+            this.updateProductButton.Size = new System.Drawing.Size(197, 50);
+            this.updateProductButton.TabIndex = 22;
+            this.updateProductButton.Text = "Update Product";
+            this.updateProductButton.UseVisualStyleBackColor = true;
+            // 
+            // portionSizeValue
+            // 
+            this.portionSizeValue.Location = new System.Drawing.Point(372, 340);
+            this.portionSizeValue.Name = "portionSizeValue";
+            this.portionSizeValue.Size = new System.Drawing.Size(77, 43);
+            this.portionSizeValue.TabIndex = 21;
+            // 
+            // portionSizeLabel
+            // 
+            this.portionSizeLabel.AutoSize = true;
+            this.portionSizeLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.portionSizeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.portionSizeLabel.Location = new System.Drawing.Point(32, 340);
+            this.portionSizeLabel.Name = "portionSizeLabel";
+            this.portionSizeLabel.Size = new System.Drawing.Size(157, 37);
+            this.portionSizeLabel.TabIndex = 20;
+            this.portionSizeLabel.Text = "Portion Size";
+            // 
+            // unitValue
+            // 
+            this.unitValue.Location = new System.Drawing.Point(317, 272);
+            this.unitValue.Name = "unitValue";
+            this.unitValue.Size = new System.Drawing.Size(132, 43);
+            this.unitValue.TabIndex = 19;
+            // 
+            // unitLabel
+            // 
+            this.unitLabel.AutoSize = true;
+            this.unitLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unitLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.unitLabel.Location = new System.Drawing.Point(32, 272);
+            this.unitLabel.Name = "unitLabel";
+            this.unitLabel.Size = new System.Drawing.Size(67, 37);
+            this.unitLabel.TabIndex = 18;
+            this.unitLabel.Text = "Unit";
+            // 
+            // portionCountValue
+            // 
+            this.portionCountValue.Location = new System.Drawing.Point(372, 200);
+            this.portionCountValue.Name = "portionCountValue";
+            this.portionCountValue.Size = new System.Drawing.Size(77, 43);
+            this.portionCountValue.TabIndex = 17;
+            // 
+            // portionCountLabel
+            // 
+            this.portionCountLabel.AutoSize = true;
+            this.portionCountLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.portionCountLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.portionCountLabel.Location = new System.Drawing.Point(32, 200);
+            this.portionCountLabel.Name = "portionCountLabel";
+            this.portionCountLabel.Size = new System.Drawing.Size(182, 37);
+            this.portionCountLabel.TabIndex = 16;
+            this.portionCountLabel.Text = "Portion Count";
+            // 
+            // productNameValue
+            // 
+            this.productNameValue.Location = new System.Drawing.Point(232, 128);
+            this.productNameValue.Name = "productNameValue";
+            this.productNameValue.Size = new System.Drawing.Size(217, 43);
+            this.productNameValue.TabIndex = 15;
+            // 
+            // productNameLabel
+            // 
+            this.productNameLabel.AutoSize = true;
+            this.productNameLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productNameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.productNameLabel.Location = new System.Drawing.Point(32, 128);
+            this.productNameLabel.Name = "productNameLabel";
+            this.productNameLabel.Size = new System.Drawing.Size(182, 37);
+            this.productNameLabel.TabIndex = 14;
+            this.productNameLabel.Text = "Product name";
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.headerLabel.Location = new System.Drawing.Point(30, 36);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(407, 50);
+            this.headerLabel.TabIndex = 13;
+            this.headerLabel.Text = "Update Selected Product";
+            // 
+            // UpdateProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(528, 555);
+            this.Controls.Add(this.updateProductButton);
+            this.Controls.Add(this.portionSizeValue);
+            this.Controls.Add(this.portionSizeLabel);
+            this.Controls.Add(this.unitValue);
+            this.Controls.Add(this.unitLabel);
+            this.Controls.Add(this.portionCountValue);
+            this.Controls.Add(this.portionCountLabel);
+            this.Controls.Add(this.productNameValue);
+            this.Controls.Add(this.productNameLabel);
+            this.Controls.Add(this.headerLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.Name = "UpdateProductForm";
+            this.Text = "Update Product";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button updateProductButton;
+        private System.Windows.Forms.TextBox portionSizeValue;
+        private System.Windows.Forms.Label portionSizeLabel;
+        private System.Windows.Forms.TextBox unitValue;
+        private System.Windows.Forms.Label unitLabel;
+        private System.Windows.Forms.TextBox portionCountValue;
+        private System.Windows.Forms.Label portionCountLabel;
+        private System.Windows.Forms.TextBox productNameValue;
+        private System.Windows.Forms.Label productNameLabel;
+        private System.Windows.Forms.Label headerLabel;
+    }
+}
