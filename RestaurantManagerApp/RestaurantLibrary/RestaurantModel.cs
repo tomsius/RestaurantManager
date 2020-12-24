aae908edd89e37b77c4608b5f54070987b378497
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 13:07:32 +0200

    Created restaurant model

---
+++ RestaurantManagerApp/RestaurantLibrary/RestaurantModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantLibrary
+{
+    public class RestaurantModel
+    {
+        /// <summary>
+        /// Represents stock of the restaurant
+        /// </summary>
+        public StockModel Stock { get; set; }
+
+        /// <summary>
+        /// Represents the menu available in the restaurant
+        /// </summary>
+        public MenuModel Menu { get; set; }
+
+        /// <summary>
+        /// Represents customers' orders
+        /// </summary>
+        public List<OrderModel> Orders { get; set; } = new List<OrderModel>();
+    }
+}
