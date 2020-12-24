e7a386ad2c0bd009e7f04285e369ef0b4de98349
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 16:19:34 +0200

    Created text connector

---
+++ RestaurantManagerApp/RestaurantLibrary/TextConnector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantLibrary
+{
+    public class TextConnector : IConnection
+    {
+        /// TODO - Implement writing to file
+        /// <summary>
+        /// Adds row to file
+        /// </summary>
+        /// <param name="product">The product information.</param>
+        /// <returns>The product information with unique identifier.</returns>
+        public ProductModel CreateProduct(ProductModel product)
+        {
+            product.Id = 1;
+
+            return product;
+        }
+    }
+}
