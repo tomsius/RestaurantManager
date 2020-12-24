f9f4205874e794c33b678719e59f94b7829d285c
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 13:07:41 +0200

    Created stock model

---
+++ RestaurantManagerApp/RestaurantLibrary/StockModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantLibrary
+{
+    public class StockModel
+    {
+        /// <summary>
+        /// Represents the products in stock
+        /// </summary>
+        public List<ProductModel> Products { get; set; } = new List<ProductModel>();
+    }
+}
