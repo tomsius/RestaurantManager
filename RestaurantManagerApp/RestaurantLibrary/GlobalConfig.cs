ed3a27a0dee27d7a1aa47048bce1dd905630a62d
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 17:55:27 +0200

    Changed global config to store one connection

--- RestaurantManagerApp/RestaurantLibrary/GlobalConfig.cs
+++ RestaurantManagerApp/RestaurantLibrary/GlobalConfig.cs
@@ -5,13 +5,13 @@ namespace RestaurantLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IConnection> Connections { get; private set; } = new List<IConnection>();
+        public static IConnection Connection { get; private set; }
 
         public static void InitializeConnections()
         {
             // TODO - Set up Text Connector properly
             TextConnector textConnector = new TextConnector();
-            Connections.Add(textConnector);
+            Connection = textConnector;
         }
     }
 }
