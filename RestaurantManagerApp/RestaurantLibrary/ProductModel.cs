e07a5b44831728632b3d873e24b4f2cb82721d62
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 13:07:16 +0200

    Created product model

---
+++ RestaurantManagerApp/RestaurantLibrary/ProductModel.cs
@@ -0,0 +1,25 @@
+﻿namespace RestaurantLibrary
+{
+    public class ProductModel
+    {
+        /// <summary>
+        /// Represent the product's name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Represent the available portion count of product
+        /// </summary>
+        public int PortionCount { get; set; }
+
+        /// <summary>
+        /// Represents the measure units of product
+        /// </summary>
+        public string Unit { get; set; }
+
+        /// <summary>
+        /// Represents the portion size of product
+        /// </summary>
+        public double PortionSize { get; set; }
+    }
+}
