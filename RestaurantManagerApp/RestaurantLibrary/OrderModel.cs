4023be6e51237c8b6f6c702afb2a53a6c4a5c3b7
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 13:07:09 +0200

    Created order model

---
+++ RestaurantManagerApp/RestaurantLibrary/OrderModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantLibrary
+{
+    public class OrderModel
+    {
+        /// <summary>
+        /// Represents the date when order was created
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Represents items ordered from the menu
+        /// </summary>
+        public List<MenuItemModel> OrderedItems { get; set; } = new List<MenuItemModel>();
+    }
+}
