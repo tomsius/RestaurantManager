1536a93b1746d0b8ffb95c7af4b2dd18a248db35
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 14:51:34 +0200

    Created orders form

---
+++ RestaurantManagerApp/RestaurantManagerUI/OrdersViewerForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace RestaurantManagerUI
+{
+    partial class OrdersViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.ordersListBox = new System.Windows.Forms.ListBox();
+            this.createOrderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.headerLabel.Location = new System.Drawing.Point(21, 18);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(127, 50);
+            this.headerLabel.TabIndex = 14;
+            this.headerLabel.Text = "Orders";
+            // 
+            // ordersListBox
+            // 
+            this.ordersListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ordersListBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ordersListBox.FormattingEnabled = true;
+            this.ordersListBox.ItemHeight = 30;
+            this.ordersListBox.Location = new System.Drawing.Point(30, 100);
+            this.ordersListBox.Name = "ordersListBox";
+            this.ordersListBox.Size = new System.Drawing.Size(482, 182);
+            this.ordersListBox.TabIndex = 19;
+            // 
+            // createOrderButton
+            // 
+            this.createOrderButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.createOrderButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.createOrderButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.createOrderButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createOrderButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createOrderButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.createOrderButton.Location = new System.Drawing.Point(30, 337);
+            this.createOrderButton.Name = "createOrderButton";
+            this.createOrderButton.Size = new System.Drawing.Size(197, 50);
+            this.createOrderButton.TabIndex = 20;
+            this.createOrderButton.Text = "Create New Order";
+            this.createOrderButton.UseVisualStyleBackColor = true;
+            // 
+            // OrdersViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(581, 424);
+            this.Controls.Add(this.createOrderButton);
+            this.Controls.Add(this.ordersListBox);
+            this.Controls.Add(this.headerLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.Name = "OrdersViewerForm";
+            this.Text = "OrdersViewerForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.ListBox ordersListBox;
+        private System.Windows.Forms.Button createOrderButton;
+    }
+}
