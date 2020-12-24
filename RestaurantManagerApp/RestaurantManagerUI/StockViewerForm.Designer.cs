2a42fc3d4acbf9c09980981b54335a140f47b1aa
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 14:01:59 +0200

    Created stock form

---
+++ RestaurantManagerApp/RestaurantManagerUI/StockViewerForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace RestaurantManagerUI
+{
+    partial class StockViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.productsListBox = new System.Windows.Forms.ListBox();
+            this.productsLabel = new System.Windows.Forms.Label();
+            this.addProductButton = new System.Windows.Forms.Button();
+            this.updateProductButton = new System.Windows.Forms.Button();
+            this.removeProductButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.headerLabel.Location = new System.Drawing.Point(25, 27);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(104, 50);
+            this.headerLabel.TabIndex = 1;
+            this.headerLabel.Text = "Stock";
+            // 
+            // productsListBox
+            // 
+            this.productsListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.productsListBox.FormattingEnabled = true;
+            this.productsListBox.ItemHeight = 30;
+            this.productsListBox.Location = new System.Drawing.Point(34, 162);
+            this.productsListBox.Name = "productsListBox";
+            this.productsListBox.Size = new System.Drawing.Size(500, 182);
+            this.productsListBox.TabIndex = 2;
+            // 
+            // productsLabel
+            // 
+            this.productsLabel.AutoSize = true;
+            this.productsLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.productsLabel.Location = new System.Drawing.Point(27, 109);
+            this.productsLabel.Name = "productsLabel";
+            this.productsLabel.Size = new System.Drawing.Size(120, 37);
+            this.productsLabel.TabIndex = 3;
+            this.productsLabel.Text = "Products";
+            // 
+            // addProductButton
+            // 
+            this.addProductButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.addProductButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.addProductButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.addProductButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addProductButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addProductButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.addProductButton.Location = new System.Drawing.Point(34, 385);
+            this.addProductButton.Name = "addProductButton";
+            this.addProductButton.Size = new System.Drawing.Size(197, 50);
+            this.addProductButton.TabIndex = 4;
+            this.addProductButton.Text = "Add New Product";
+            this.addProductButton.UseVisualStyleBackColor = true;
+            // 
+            // updateProductButton
+            // 
+            this.updateProductButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.updateProductButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.updateProductButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.updateProductButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.updateProductButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateProductButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.updateProductButton.Location = new System.Drawing.Point(34, 471);
+            this.updateProductButton.Name = "updateProductButton";
+            this.updateProductButton.Size = new System.Drawing.Size(197, 50);
+            this.updateProductButton.TabIndex = 5;
+            this.updateProductButton.Text = "Update Selected";
+            this.updateProductButton.UseVisualStyleBackColor = true;
+            // 
+            // removeProductButton
+            // 
+            this.removeProductButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.removeProductButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.removeProductButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.removeProductButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.removeProductButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeProductButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.removeProductButton.Location = new System.Drawing.Point(34, 559);
+            this.removeProductButton.Name = "removeProductButton";
+            this.removeProductButton.Size = new System.Drawing.Size(197, 50);
+            this.removeProductButton.TabIndex = 6;
+            this.removeProductButton.Text = "Remove Selected";
+            this.removeProductButton.UseVisualStyleBackColor = true;
+            // 
+            // StockViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(830, 678);
+            this.Controls.Add(this.removeProductButton);
+            this.Controls.Add(this.updateProductButton);
+            this.Controls.Add(this.addProductButton);
+            this.Controls.Add(this.productsLabel);
+            this.Controls.Add(this.productsListBox);
+            this.Controls.Add(this.headerLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "StockViewerForm";
+            this.Text = "Stock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.ListBox productsListBox;
+        private System.Windows.Forms.Label productsLabel;
+        private System.Windows.Forms.Button addProductButton;
+        private System.Windows.Forms.Button updateProductButton;
+        private System.Windows.Forms.Button removeProductButton;
+    }
+}
