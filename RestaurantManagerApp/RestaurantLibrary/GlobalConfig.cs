abe08aadc28af6f3d70716b61a9a9ec571fed3a9
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 16:18:23 +0200

    Created global config to store connections

---
+++ RestaurantManagerApp/RestaurantLibrary/GlobalConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IConnection> Connections { get; private set; } = new List<IConnection>();
+
+        public static void InitializeConnections()
+        {
+            // TODO - Set up Text Connector properly
+            TextConnector textConnector = new TextConnector();
+            Connections.Add(textConnector);
+        }
+    }
+}
