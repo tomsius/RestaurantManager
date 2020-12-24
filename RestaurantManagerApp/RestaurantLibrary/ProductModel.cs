3909d86774b66bc0835715b94a305ed2e9a5dd30
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 17:08:08 +0200

    Added constructor to ProductModel

--- RestaurantManagerApp/RestaurantLibrary/ProductModel.cs
+++ RestaurantManagerApp/RestaurantLibrary/ProductModel.cs
@@ -26,5 +26,18 @@
         /// Represents the portion size of product
         /// </summary>
         public double PortionSize { get; set; }
+
+        public ProductModel(string productName, string portionCount, string unit, string portionSize)
+        {
+            Name = productName;
+
+            int portionCountValue = int.Parse(portionCount);
+            PortionCount = portionCountValue;
+
+            Unit = unit;
+
+            double portionSizeValue = double.Parse(portionSize);
+            PortionSize = portionSizeValue;
+        }
     }
 }
