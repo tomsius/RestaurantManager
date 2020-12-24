2c1169ea4b1945dc85f775b1e2c6b94a5af7210c
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 14:26:17 +0200

    Created menu form

---
+++ RestaurantManagerApp/RestaurantManagerUI/MenuViewerForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace RestaurantManagerUI
+{
+    partial class MenuViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuItemsLabel = new System.Windows.Forms.Label();
+            this.menuItemsListBox = new System.Windows.Forms.ListBox();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.removeMenuItemButton = new System.Windows.Forms.Button();
+            this.updateMenuItemButton = new System.Windows.Forms.Button();
+            this.addMenuItemButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // menuItemsLabel
+            // 
+            this.menuItemsLabel.AutoSize = true;
+            this.menuItemsLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuItemsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.menuItemsLabel.Location = new System.Drawing.Point(26, 110);
+            this.menuItemsLabel.Name = "menuItemsLabel";
+            this.menuItemsLabel.Size = new System.Drawing.Size(156, 37);
+            this.menuItemsLabel.TabIndex = 6;
+            this.menuItemsLabel.Text = "Menu Items";
+            // 
+            // menuItemsListBox
+            // 
+            this.menuItemsListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.menuItemsListBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuItemsListBox.FormattingEnabled = true;
+            this.menuItemsListBox.ItemHeight = 30;
+            this.menuItemsListBox.Location = new System.Drawing.Point(33, 163);
+            this.menuItemsListBox.Name = "menuItemsListBox";
+            this.menuItemsListBox.Size = new System.Drawing.Size(586, 182);
+            this.menuItemsListBox.TabIndex = 5;
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.headerLabel.Location = new System.Drawing.Point(24, 28);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(112, 50);
+            this.headerLabel.TabIndex = 4;
+            this.headerLabel.Text = "Menu";
+            // 
+            // removeMenuItemButton
+            // 
+            this.removeMenuItemButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.removeMenuItemButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.removeMenuItemButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.removeMenuItemButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.removeMenuItemButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeMenuItemButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.removeMenuItemButton.Location = new System.Drawing.Point(33, 564);
+            this.removeMenuItemButton.Name = "removeMenuItemButton";
+            this.removeMenuItemButton.Size = new System.Drawing.Size(197, 50);
+            this.removeMenuItemButton.TabIndex = 9;
+            this.removeMenuItemButton.Text = "Remove Selected";
+            this.removeMenuItemButton.UseVisualStyleBackColor = true;
+            // 
+            // updateMenuItemButton
+            // 
+            this.updateMenuItemButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.updateMenuItemButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.updateMenuItemButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.updateMenuItemButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.updateMenuItemButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateMenuItemButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.updateMenuItemButton.Location = new System.Drawing.Point(33, 476);
+            this.updateMenuItemButton.Name = "updateMenuItemButton";
+            this.updateMenuItemButton.Size = new System.Drawing.Size(197, 50);
+            this.updateMenuItemButton.TabIndex = 8;
+            this.updateMenuItemButton.Text = "Update Selected";
+            this.updateMenuItemButton.UseVisualStyleBackColor = true;
+            // 
+            // addMenuItemButton
+            // 
+            this.addMenuItemButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.addMenuItemButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.addMenuItemButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.addMenuItemButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addMenuItemButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addMenuItemButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.addMenuItemButton.Location = new System.Drawing.Point(33, 390);
+            this.addMenuItemButton.Name = "addMenuItemButton";
+            this.addMenuItemButton.Size = new System.Drawing.Size(197, 50);
+            this.addMenuItemButton.TabIndex = 7;
+            this.addMenuItemButton.Text = "Add New Menu Item";
+            this.addMenuItemButton.UseVisualStyleBackColor = true;
+            // 
+            // MenuViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(660, 682);
+            this.Controls.Add(this.removeMenuItemButton);
+            this.Controls.Add(this.updateMenuItemButton);
+            this.Controls.Add(this.addMenuItemButton);
+            this.Controls.Add(this.menuItemsLabel);
+            this.Controls.Add(this.menuItemsListBox);
+            this.Controls.Add(this.headerLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.Name = "MenuViewerForm";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label menuItemsLabel;
+        private System.Windows.Forms.ListBox menuItemsListBox;
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Button removeMenuItemButton;
+        private System.Windows.Forms.Button updateMenuItemButton;
+        private System.Windows.Forms.Button addMenuItemButton;
+    }
+}
