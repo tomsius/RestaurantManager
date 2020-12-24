be00f22f57988c3664333c1f12ce5631a3571de3
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-24 14:44:53 +0200

    Created menu item update form

---
+++ RestaurantManagerApp/RestaurantManagerUI/UpdateMenuItemForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace RestaurantManagerUI
+{
+    partial class UpdateMenuItemForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.updateMenuItemButton = new System.Windows.Forms.Button();
+            this.removeIngredientButton = new System.Windows.Forms.Button();
+            this.ingredientsListBox = new System.Windows.Forms.ListBox();
+            this.ingredientsDropDown = new System.Windows.Forms.ComboBox();
+            this.selectIngredientsLabel = new System.Windows.Forms.Label();
+            this.menuItemNameValue = new System.Windows.Forms.TextBox();
+            this.menuItemNameLabel = new System.Windows.Forms.Label();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // updateMenuItemButton
+            // 
+            this.updateMenuItemButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.updateMenuItemButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.updateMenuItemButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.updateMenuItemButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.updateMenuItemButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateMenuItemButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.updateMenuItemButton.Location = new System.Drawing.Point(34, 695);
+            this.updateMenuItemButton.Name = "updateMenuItemButton";
+            this.updateMenuItemButton.Size = new System.Drawing.Size(220, 50);
+            this.updateMenuItemButton.TabIndex = 20;
+            this.updateMenuItemButton.Text = "Update Menu Item";
+            this.updateMenuItemButton.UseVisualStyleBackColor = true;
+            // 
+            // removeIngredientButton
+            // 
+            this.removeIngredientButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.removeIngredientButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.removeIngredientButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.removeIngredientButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.removeIngredientButton.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeIngredientButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.removeIngredientButton.Location = new System.Drawing.Point(34, 577);
+            this.removeIngredientButton.Name = "removeIngredientButton";
+            this.removeIngredientButton.Size = new System.Drawing.Size(197, 50);
+            this.removeIngredientButton.TabIndex = 19;
+            this.removeIngredientButton.Text = "Remove Selected";
+            this.removeIngredientButton.UseVisualStyleBackColor = true;
+            // 
+            // ingredientsListBox
+            // 
+            this.ingredientsListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ingredientsListBox.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ingredientsListBox.FormattingEnabled = true;
+            this.ingredientsListBox.ItemHeight = 30;
+            this.ingredientsListBox.Location = new System.Drawing.Point(34, 367);
+            this.ingredientsListBox.Name = "ingredientsListBox";
+            this.ingredientsListBox.Size = new System.Drawing.Size(446, 182);
+            this.ingredientsListBox.TabIndex = 18;
+            // 
+            // ingredientsDropDown
+            // 
+            this.ingredientsDropDown.FormattingEnabled = true;
+            this.ingredientsDropDown.Location = new System.Drawing.Point(34, 267);
+            this.ingredientsDropDown.Name = "ingredientsDropDown";
+            this.ingredientsDropDown.Size = new System.Drawing.Size(446, 45);
+            this.ingredientsDropDown.TabIndex = 17;
+            // 
+            // selectIngredientsLabel
+            // 
+            this.selectIngredientsLabel.AutoSize = true;
+            this.selectIngredientsLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectIngredientsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.selectIngredientsLabel.Location = new System.Drawing.Point(27, 206);
+            this.selectIngredientsLabel.Name = "selectIngredientsLabel";
+            this.selectIngredientsLabel.Size = new System.Drawing.Size(227, 37);
+            this.selectIngredientsLabel.TabIndex = 16;
+            this.selectIngredientsLabel.Text = "Select Ingredients";
+            // 
+            // menuItemNameValue
+            // 
+            this.menuItemNameValue.Location = new System.Drawing.Point(263, 126);
+            this.menuItemNameValue.Name = "menuItemNameValue";
+            this.menuItemNameValue.Size = new System.Drawing.Size(217, 43);
+            this.menuItemNameValue.TabIndex = 15;
+            // 
+            // menuItemNameLabel
+            // 
+            this.menuItemNameLabel.AutoSize = true;
+            this.menuItemNameLabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuItemNameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.menuItemNameLabel.Location = new System.Drawing.Point(27, 126);
+            this.menuItemNameLabel.Name = "menuItemNameLabel";
+            this.menuItemNameLabel.Size = new System.Drawing.Size(88, 37);
+            this.menuItemNameLabel.TabIndex = 14;
+            this.menuItemNameLabel.Text = "Name";
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.headerLabel.Location = new System.Drawing.Point(25, 20);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(455, 50);
+            this.headerLabel.TabIndex = 13;
+            this.headerLabel.Text = "Update Selected Menu Item";
+            // 
+            // UpdateMenuItemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(512, 782);
+            this.Controls.Add(this.updateMenuItemButton);
+            this.Controls.Add(this.removeIngredientButton);
+            this.Controls.Add(this.ingredientsListBox);
+            this.Controls.Add(this.ingredientsDropDown);
+            this.Controls.Add(this.selectIngredientsLabel);
+            this.Controls.Add(this.menuItemNameValue);
+            this.Controls.Add(this.menuItemNameLabel);
+            this.Controls.Add(this.headerLabel);
+            this.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.Name = "UpdateMenuItemForm";
+            this.Text = "Update Menu Item";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button updateMenuItemButton;
+        private System.Windows.Forms.Button removeIngredientButton;
+        private System.Windows.Forms.ListBox ingredientsListBox;
+        private System.Windows.Forms.ComboBox ingredientsDropDown;
+        private System.Windows.Forms.Label selectIngredientsLabel;
+        private System.Windows.Forms.TextBox menuItemNameValue;
+        private System.Windows.Forms.Label menuItemNameLabel;
+        private System.Windows.Forms.Label headerLabel;
+    }
+}
