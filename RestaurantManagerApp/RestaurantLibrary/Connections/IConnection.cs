e6e329af2371b64f63e58ce59ba78eda8b0cf089
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-25 15:57:16 +0200

    Added methods to IConnection

--- RestaurantManagerApp/RestaurantLibrary/Connections/IConnection.cs
+++ RestaurantManagerApp/RestaurantLibrary/Connections/IConnection.cs
@@ -1,9 +1,12 @@
 ﻿using RestaurantLibrary.Models;
+using System.Collections.Generic;
 
 namespace RestaurantLibrary.Connections
 {
     public interface IConnection
     {
         ProductModel CreateProduct(ProductModel product);
+        List<ProductModel> GetAllProducts();
+        MenuItemModel CreateMenuItem(MenuItemModel menuItem);
     }
 }
