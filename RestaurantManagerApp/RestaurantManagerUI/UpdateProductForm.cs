ddd00cdb12dc15ebd4d6979c52b5cee9b1138cfc
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-27 11:44:22 +0200

    Added field validation in product update form

--- RestaurantManagerApp/RestaurantManagerUI/UpdateProductForm.cs
+++ RestaurantManagerApp/RestaurantManagerUI/UpdateProductForm.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantLibrary.Models;
+﻿using RestaurantLibrary;
+using RestaurantLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -34,7 +35,112 @@ namespace RestaurantManagerUI
 
         private void updateProductButton_Click(object sender, EventArgs e)
         {
+            if (AreFormInputsValid())
+            {
+                ProductModel newProduct = new ProductModel(productNameValue.Text, portionCountValue.Text, unitValue.Text, portionSizeValue.Text);
+                newProduct.Id = product.Id;
 
+                GlobalConfig.Connection.UpdateProduct(newProduct);
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Form.");
+            }
+        }
+
+        private bool AreFormInputsValid()
+        {
+            bool isFormValid = true;
+
+            if (!IsNameValid())
+            {
+                isFormValid = false;
+            }
+
+            if (!IsPortionCountValid())
+            {
+                isFormValid = false;
+            }
+
+            if (!IsUnitValid())
+            {
+                isFormValid = false;
+            }
+
+            if (!IsPortionSizeValid())
+            {
+                isFormValid = false;
+            }
+
+            return isFormValid;
+        }
+
+        private bool IsNameValid()
+        {
+            if (productNameValue.Text.Length == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsPortionCountValid()
+        {
+            int portionCount = 0;
+            bool isPortionCountValid = int.TryParse(portionCountValue.Text, out portionCount);
+
+            if (!isPortionCountValid)
+            {
+                return false;
+            }
+
+            if (portionCount < 1)
+            {
+                return false;
+            }
+
+            if (portionCountValue.Text.Length == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsUnitValid()
+        {
+            if (unitValue.Text.Length == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsPortionSizeValid()
+        {
+            double portionSize = 0.0;
+            bool isPortionSizeValid = double.TryParse(portionSizeValue.Text, out portionSize);
+
+            if (!isPortionSizeValid)
+            {
+                return false;
+            }
+
+            if (portionSize < 0)
+            {
+                return false;
+            }
+
+            if (portionSizeValue.Text.Length == 0)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
