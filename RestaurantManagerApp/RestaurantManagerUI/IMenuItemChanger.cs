52c3a929a6ff5d53add43211657f867a508ff940
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-27 13:10:22 +0200

    Created interface for updating menu item

---
+++ RestaurantManagerApp/RestaurantManagerUI/IMenuItemChanger.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagerUI
+{
+    public interface IMenuItemChanger
+    {
+        void CompleteMenuItemUpdate(MenuItemModel newMenuItem);
+    }
+}
