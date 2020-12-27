3ee893174da1243cc1a20381a7b437362c110cdc
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-27 17:36:09 +0200

    Fixed stock list not updating after updating product

--- RestaurantManagerApp/RestaurantManagerUI/UpdateProductForm.cs
+++ RestaurantManagerApp/RestaurantManagerUI/UpdateProductForm.cs
@@ -42,6 +42,7 @@ namespace RestaurantManagerUI
                 ProductModel newProduct = new ProductModel(productNameValue.Text, portionCountValue.Text, unitValue.Text, portionSizeValue.Text);
 
                 GlobalConfig.Connection.UpdateProduct(product.Id, newProduct);
+                newProduct.Id = product.Id;
 
                 callingFrom.CompleteProductUpdate(newProduct);
 
