8de3592fe9eeaac0afbdc0b5dd166f30f9bf45e2
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-27 14:17:04 +0200

    Add order initialization  of menu items

--- RestaurantManagerApp/RestaurantManagerUI/AddOrderForm.cs
+++ RestaurantManagerApp/RestaurantManagerUI/AddOrderForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestaurantLibrary;
+using RestaurantLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,25 @@ namespace RestaurantManagerUI
 {
     public partial class AddOrderForm : Form
     {
+        private List<MenuItemModel> availableMenuItems = GlobalConfig.Connection.GetAllMenuItems();
+        private List<MenuItemModel> selectedMenuItems = new List<MenuItemModel>();
+
         public AddOrderForm()
         {
             InitializeComponent();
+
+            WireUpLists();
+        }
+
+        private void WireUpLists()
+        {
+            menuItemsDropDown.DataSource = null;
+            menuItemsDropDown.DataSource = availableMenuItems;
+            menuItemsDropDown.DisplayMember = "Name";
+
+            menuItemsListBox.DataSource = null;
+            menuItemsListBox.DataSource = selectedMenuItems;
+            menuItemsListBox.DisplayMember = "Name";
         }
     }
 }
