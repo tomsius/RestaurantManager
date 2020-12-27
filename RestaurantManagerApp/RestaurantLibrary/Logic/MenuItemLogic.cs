62a7aa8477ef03e041b91fe7b8a6a3ee579a5c2c
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-27 18:27:24 +0200

    Created menu item logic

---
+++ RestaurantManagerApp/RestaurantLibrary/Logic/MenuItemLogic.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantLibrary.Logic
+{
+    public static class MenuItemLogic
+    {
+        public static List<ProductModel> GetNotUsedIngredients(MenuItemModel menuItem)
+        {
+            List<ProductModel> allIngredients = GlobalConfig.Connection.GetAllProducts();
+            List<ProductModel> notUsedIngredients = new List<ProductModel>();
+
+            foreach (ProductModel product in allIngredients)
+            {
+                bool contains = false;
+                foreach (ProductModel ingredient in menuItem.Ingredients)
+                {
+                    if (product.Id == ingredient.Id)
+                    {
+                        contains = true;
+                        break;
+                    }
+                }
+
+                if (!contains)
+                {
+                    notUsedIngredients.Add(product);
+                }
+            }
+
+            return notUsedIngredients;
+        }
+    }
+}
