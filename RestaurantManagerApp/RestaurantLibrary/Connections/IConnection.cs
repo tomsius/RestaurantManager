0a827d5fed1b35286307763cb42fc8a4f432ffb3
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-27 15:25:37 +0200

    Added methods to create order, get all orders and update products in stock

--- RestaurantManagerApp/RestaurantLibrary/Connections/IConnection.cs
+++ RestaurantManagerApp/RestaurantLibrary/Connections/IConnection.cs
@@ -7,10 +7,13 @@ namespace RestaurantLibrary.Connections
     {
         ProductModel CreateProduct(ProductModel product);
         MenuItemModel CreateMenuItem(MenuItemModel menuItem);
+        OrderModel CreateOrder(OrderModel order);
         List<ProductModel> GetAllProducts();
         List<MenuItemModel> GetAllMenuItems();
+        List<OrderModel> GetAllOrders();
         ProductModel UpdateProduct(int id, ProductModel newProduct);
         MenuItemModel UpdateMenuItem(int id, MenuItemModel newMenuItem);
+        void UpdateProductStock(List<ProductModel> products);
         void DeleteProduct(int id);
         void DeleteMenuItem(int id);
     }
