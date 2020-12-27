7fdbf51e83984ad629bb9d30bb72e4b50f55dd22
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-27 13:10:36 +0200

    Created interface for adding menu item

---
+++ RestaurantManagerApp/RestaurantManagerUI/IMenuItemRequester.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagerUI
+{
+    public interface IMenuItemRequester
+    {
+        void CompleteMenuItemCreation(MenuItemModel menuItem);
+    }
+}
