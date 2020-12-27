820027683aa61f5cb1cbc9ca8079bbe741869b2b
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-27 11:33:11 +0200

    Added update methods for produtcs and menu items

--- RestaurantManagerApp/RestaurantLibrary/Connections/IConnection.cs
+++ RestaurantManagerApp/RestaurantLibrary/Connections/IConnection.cs
@@ -6,7 +6,10 @@ namespace RestaurantLibrary.Connections
     public interface IConnection
     {
         ProductModel CreateProduct(ProductModel product);
-        List<ProductModel> GetAllProducts();
         MenuItemModel CreateMenuItem(MenuItemModel menuItem);
+        List<ProductModel> GetAllProducts();
+        ProductModel UpdateProduct(ProductModel product);
+        MenuItemModel UpdateMenuItem(MenuItemModel menuItem);
+
     }
 }
