75ec6efd3551d5d0f366937b9b30193357b66ff8
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-27 13:11:11 +0200

    Initializatino of menu items and added functionality to add menu item button

--- RestaurantManagerApp/RestaurantManagerUI/MenuViewerForm.cs
+++ RestaurantManagerApp/RestaurantManagerUI/MenuViewerForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RestaurantLibrary;
+using RestaurantLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +12,40 @@ using System.Windows.Forms;
 
 namespace RestaurantManagerUI
 {
-    public partial class MenuViewerForm : Form
+    public partial class MenuViewerForm : Form, IMenuItemRequester, IMenuItemChanger
     {
+        private List<MenuItemModel> menuItems = GlobalConfig.Connection.GetAllMenuItems();
+
         public MenuViewerForm()
         {
             InitializeComponent();
+
+            WireUpLists();
+        }
+
+        private void WireUpLists()
+        {
+            menuItemsListBox.DataSource = null;
+            menuItemsListBox.DataSource = menuItems;
+            menuItemsListBox.DisplayMember = "DisplayName";
+        }
+
+        private void addMenuItemButton_Click(object sender, EventArgs e)
+        {
+            AddMenuItemForm form = new AddMenuItemForm(this);
+            form.Show();
+        }
+
+        public void CompleteMenuItemCreation(MenuItemModel menuItem)
+        {
+            menuItems.Add(menuItem);
+
+            WireUpLists();
+        }
+
+        public void CompleteMenuItemUpdate(MenuItemModel newMenuItem)
+        {
+            throw new NotImplementedException();
         }
     }
 }
