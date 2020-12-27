74c8db3f8dd419fa865b5a89859716f8223a6d06
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-27 15:24:35 +0200

    Created stock logic to check for products in stock and reduce its amount

---
+++ RestaurantManagerApp/RestaurantLibrary/StockLogic.cs
@@ -0,0 +1,22 @@
+﻿using RestaurantLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantLibrary
+{
+    public static class StockLogic
+    {
+        public static bool IsProductInStock(List<ProductModel> products, int id)
+        {
+            return products.Where(p => p.Id == id).First().PortionCount - 1 >= 0;
+        }
+
+        public static void ReduceProductInStock(List<ProductModel> products, int id)
+        {
+            products.Where(p => p.Id == id).First().PortionCount--;
+        }
+    }
+}
