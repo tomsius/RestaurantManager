4417916b236d091ae05155958fcae2bc7d95c64f
Tomas Kaselynas <tomas.kaselynas@ktu.edu>
2020-12-28 15:08:16 +0200

    Added check for empty order menu item lists in order logic

--- RestaurantManagerApp/RestaurantLibrary/Logic/OrderLogic.cs
+++ RestaurantManagerApp/RestaurantLibrary/Logic/OrderLogic.cs
@@ -10,12 +10,17 @@ namespace RestaurantLibrary
     public static class OrderLogic
     {
         /// <summary>
-        /// Check if all ordered menu items' ingredients are in stock.
+        /// Check if all ordered menu items' ingredients are in stock. Throws ArgumentException if empty list is given.
         /// </summary>
         /// <param name="orderedMenuItems">Ordered menu items of order.</param>
         /// <returns></returns>
         public static bool AreOrderedMenuItemsProductsInStock(List<MenuItemModel> orderedMenuItems)
         {
+            if (orderedMenuItems.Count == 0)
+            {
+                throw new ArgumentException("Ordered menu item list can not be empty.");
+            }
+
             foreach (MenuItemModel menuItem in orderedMenuItems)
             {
                 foreach (ProductModel product in menuItem.Ingredients)
@@ -35,11 +40,16 @@ namespace RestaurantLibrary
         }
 
         /// <summary>
-        /// Goes through all of the order item's ingredients and reduces them by 1 in stock.
+        /// Goes through all of the order item's ingredients and reduces them by 1 in stock. Throws ArgumentException if empty list is given.
         /// </summary>
         /// <param name="orderedMenuItems">Ordered menu items of order.</param>
         public static void ReduceProductsInStock(List<MenuItemModel> orderedMenuItems)
         {
+            if (orderedMenuItems.Count == 0)
+            {
+                throw new ArgumentException("Ordered menu item list can not be empty.");
+            }
+
             List<ProductModel> productsInStock = GlobalConfig.Connection.GetAllProducts();
 
             foreach (MenuItemModel menuItem in orderedMenuItems)
